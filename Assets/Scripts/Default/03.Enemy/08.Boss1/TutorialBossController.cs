a8e7d933daaf35224e9c7cdf08c2cd58ee295382
lMalaria <shinobi441@gmail.com>
2018-03-14 17:40:27 +0900

    Made Boss Script
    Couldn't find correct model

---
+++ Assets/Scripts/Default/03.Enemy/08.Boss1/TutorialBossController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using debug = UnityEngine.Debug;
+
+public class TutorialBossController : MonoBehaviour {
+    
+    public enum BossState
+    {
+        Idle = 0,
+        Die
+    }
+
+    BossState bossState;
+
+    private GameObject player;
+
+    private Stopwatch sw = new Stopwatch();
+
+    Animator animator;
+
+    private float BossHP;
+
+    void Awake()
+    {
+        player = GameObject.Find("SWAT");
+        animator = GetComponent<Animator>();
+
+        BossHP = 100;
+    }
+
+	void Start ()
+    {
+        sw.Reset();
+        bossState = BossState.Idle;
+
+	}
+	
+	void Update ()
+    {
+        BossFSM();
+	}
+
+    void BossFSM()
+    {
+        Vector3 direction = player.transform.position - this.transform.position;
+        direction.y = 0;
+
+        switch (bossState)
+        {
+            case BossState.Idle:
+
+                break;
+            case BossState.Die:
+
+                break;
+        }
+    }
+}
