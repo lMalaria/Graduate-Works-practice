35f46d3a2b40115feb0865534e6a5f8a78c3b6c5
lMalaria <shinobi441@gmail.com>
2018-03-14 17:38:21 +0900

    Hand-made CutScene Animation & Animator
    Added Scenes and Scripts for parachute scene(CutScene Camera and Parachute Scripts)

---
+++ Assets/Scripts/Default/01.Player/Parachute.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parachute : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cameraObject;
+
+    Rigidbody rigidbody;
+
+    private float dialogueEndTime;
+
+    //[SerializeField]
+    //Camera[] cameras;
+
+    void Awake()
+    {
+        //cameraObject = GameObject.Find("Camera");
+        rigidbody = GetComponent<Rigidbody>();
+
+        // 연출을 위해서 3인칭 카메라와 중력적용을 꺼 놓은 채 시작 
+        cameraObject.GetComponent<CameraFollow>().enabled = false;
+        cameraObject.GetComponent<CutSceneCamera>().enabled = false;
+        rigidbody.useGravity = false;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation; // 충돌시 물체가 돌아가지 않게 설정
+    }
+
+	void Start ()
+    {
+        //this.gameObject.transform.Rotate(90, 0, 0);
+    }
+	
+	void Update ()
+    {
+        dialogueEndTime += Time.deltaTime;
+
+        if (dialogueEndTime > 13 /*dialogueEnd*/ )
+        {
+            cameraObject.GetComponent<CutSceneCamera>().enabled = true;
+            //rigidbody.drag = 1;
+            rigidbody.useGravity = true;           
+        }
+
+        if (this.gameObject.transform.position.y < 400)
+            rigidbody.drag = 1;
+
+        if (this.gameObject.transform.position.y < 20)
+        {
+            rigidbody.drag = 0;
+            cameraObject.GetComponent<CutSceneCamera>().enabled = false;
+            cameraObject.GetComponent<CameraFollow>().enabled = true;
+        }
+
+	}
+}
---
+++ Assets/Scripts/Default/CutScene/CutSceneCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneCamera : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    public Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace;
+
+    [SerializeField]
+    private bool isLookAt;
+
+    private Vector3 thirdAngle;
+
+    private Vector3 velocity;
+
+    void Awake()
+    {
+        Cursor.visible = false;
+        thirdAngle = offsetPosition;
+        velocity = Vector3.zero;
+        isLookAt = false;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        //World 혹은 Self를 선택할 수 있는데 여기서 월드는 아마 쓰지 않을 것 같다 cf)나중에 쓸 일이 생길 것 같아서 따로 만듬
+        if (offsetPositionSpace == Space.Self)
+            transform.position = target.TransformPoint(offsetPosition);
+        else
+            transform.position = target.position + offsetPosition;
+
+        //나중에 필요 하다고 생각하여 만들어 놓은 LookAt 함수 cf)캐릭터 주위를 도는 카메라 생성
+        if (isLookAt)
+            transform.LookAt(target);
+        else
+            transform.rotation = Quaternion.Euler(35,0,0);
+    }
+}
