a191666144cc8adc62a006acb2b9f207464cd1fa
lMalaria <shinobi441@gmail.com>
2018-03-24 17:12:18 +0900

    Tried to make SpawnManager(On Processing)

---
+++ Assets/Scripts/Default/Items/Spawn/ItemSpawnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject item;
+
+    [SerializeField]
+    private GameObject itemInstance;
+
+    private Vector3 itemPosition;
+
+	void Start ()
+    {
+        itemPosition = item.transform.position;
+        InvokeRepeating("CheckItem", 1, 3);
+	}
+
+	void Update ()
+    {
+        if (Input.GetKeyDown("q"))
+            Destroy(item);
+	}
+
+    void SpawnItem(Vector3 spawnedPosition)
+    {
+        Instantiate(item, spawnedPosition, Quaternion.identity);
+    }
+
+    string Item2String(GameObject item)
+    {
+        var itemName = item.ToString();
+
+        return GameObject.Find(itemName).ToString();
+    }
+
+    void CheckItem()
+    {
+        
+        GameObject colaInstance;
+
+        //if (GameObject.Find("Cola Can") == null)
+        if (item == null)
+        {
+            colaInstance = Instantiate(item, itemPosition, Quaternion.identity);
+        }
+        
+
+    }
+}
