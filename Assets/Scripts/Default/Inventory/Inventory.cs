515ec044854c4c0709f2dcebd1a85740f2b665c0
lMalaria <shinobi441@gmail.com>
2018-03-24 17:11:28 +0900

    Tried to complete Inventory Script(On Processing)

---
+++ Assets/Scripts/Default/Inventory/Inventory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private List<Item> inventory = new List<Item>();
+
+    private bool inventoryIsOnScreen;
+
+    private Canvas inventoryCanvas;
+
+	void Start ()
+    {
+        inventoryIsOnScreen = false;
+
+        inventory.Add( new Weapon("Pistol", "basic Pistol", false, 5, 1000, gameObject));
+        //inventory.Add( new Weapon("M4", "low Recoil and good RPM(Round Per Minute) but weak", false, 10, 3000, gameObject));
+        //inventory.Add( new Weapon("AK", "high Recoil and good RPM(Round Per Minute) and very powerful", false, 15, 3500, gameObject));
+        
+        inventory.Add( new Potion("HealthPotion", "This is a Health Potion", true, 20, 200, gameObject));
+    }
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown("i") || Input.GetKeyDown("tab"))
+            inventoryIsOnScreen = !inventoryIsOnScreen;
+
+        if (inventoryIsOnScreen)
+            inventoryCanvas.enabled = true;
+        else
+            inventoryCanvas.enabled = false;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if(hit.transform.tag == "slot")
+            {
+                print(hit.transform.name);
+            }
+        }
+
+    }
+}
