05cff961e37cc73d8c6788b82033454eed1d525c
lMalaria <shinobi441@gmail.com>
2018-04-20 18:27:33 +0900

    Item Manager Script
    It is literally Manager which helps us add Script to gameObject which contained tag "item".

---
+++ Assets/Scripts/Default/Items/ItemManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+    private GameObject[] Items;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Items = GameObject.FindGameObjectsWithTag("Item");
+        
+        for(int i = 0; i< Items.Length; i++)
+        {
+            Items[i].AddComponent<CollectableItems>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
