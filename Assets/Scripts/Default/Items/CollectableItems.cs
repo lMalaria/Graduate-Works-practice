77fc658a81047501d2ac1e75d05dab01b3f2eca3
lMalaria <shinobi441@gmail.com>
2018-04-20 18:25:55 +0900

    Created Item Collider(will be fixed later)
    Above all, i created this script by using Destroy.
    But later i will Changed this script by using Renderer.enabled

---
+++ Assets/Scripts/Default/Items/CollectableItems.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableItems : MonoBehaviour {
+
+    private BoxCollider boxCollider;
+
+    void Start ()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            boxCollider.isTrigger = true;
+            //Destroy(this.gameObject);
+        }
+    }
+
+}
