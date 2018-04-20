863815842e35ecae4bb2801c9bbf2fcc7eaa9a8e
lMalaria <shinobi441@gmail.com>
2018-04-20 18:20:38 +0900

    Created SoundManager
    Completed PlaySound as a Singleton

---
+++ Assets/Scripts/Default/Manager/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager Instance = null;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        else
+        {
+            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            Destroy(gameObject);
+        }
+
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
+
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void PlayBGM(AudioSource bgmAudioSource, AudioClip bgmClip, bool isLoop, float soundVolume)
+    {
+        bgmAudioSource.clip = bgmClip;
+        bgmAudioSource.loop = isLoop;
+        bgmAudioSource.volume = soundVolume;
+        bgmAudioSource.Play();
+    }
+
+    public void PlaySound(AudioClip clip, GameObject objectToPlayOn)
+    {
+        //AudioSource.PlayClipAtPoint(clip, objectToPlayOn.transform.position);
+        AudioSource.PlayClipAtPoint(clip, objectToPlayOn.transform.position);
+    }
+}
