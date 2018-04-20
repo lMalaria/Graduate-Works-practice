1f06d48d9c90ff76dc082f8058f96a61afa9a4ab
lMalaria <shinobi441@gmail.com>
2018-04-20 18:37:11 +0900

    Inventory Script(half done)
    completed 3d model to UI

--- Assets/Scripts/Default/Inventory/Inventory.cs
+++ Assets/Scripts/Default/Inventory/Inventory.cs
@@ -1,46 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
-    private List<Item> inventory = new List<Item>();
+    //public Dictionary<string, int> items = new Dictionary<string, int>();
 
-    private bool inventoryIsOnScreen;
+    public bool inventoryIsOnScreen;
 
+    [SerializeField]
     private Canvas inventoryCanvas;
 
-	void Start ()
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private GameObject characterUI;
+
+    private Vector3 characterUIScreenPosition;
+
+    private Quaternion characterUIScreenRotation;
+
+    private GameObject[] weaponsUI;
+
+
+
+    void Start ()
     {
+        inventoryCanvas.enabled = false;
+        characterUI.SetActive(false);
         inventoryIsOnScreen = false;
 
-        inventory.Add( new Weapon("Pistol", "basic Pistol", false, 5, 1000, gameObject));
-        //inventory.Add( new Weapon("M4", "low Recoil and good RPM(Round Per Minute) but weak", false, 10, 3000, gameObject));
-        //inventory.Add( new Weapon("AK", "high Recoil and good RPM(Round Per Minute) and very powerful", false, 15, 3500, gameObject));
-        
-        inventory.Add( new Potion("HealthPotion", "This is a Health Potion", true, 20, 200, gameObject));
+        characterUI.transform.localEulerAngles = new Vector3(0, 180, 0);
+        characterUIScreenRotation = characterUI.transform.localRotation;
+
+
+
+        weaponsUI = GameObject.FindGameObjectsWithTag("Weapon");
     }
 	
 	void Update ()
     {
         if (Input.GetKeyDown("i") || Input.GetKeyDown("tab"))
             inventoryIsOnScreen = !inventoryIsOnScreen;
+        
+
 
         if (inventoryIsOnScreen)
-            inventoryCanvas.enabled = true;
-        else
-            inventoryCanvas.enabled = false;
-
-        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if(Physics.Raycast(ray, out hit))
         {
-            if(hit.transform.tag == "slot")
-            {
-                print(hit.transform.name);
-            }
+            inventoryCanvas.enabled = true;
+            characterUI.SetActive(true);
+        }
+        else
+        {
+            inventoryCanvas.enabled = false;
+            characterUI.SetActive(false);
         }
 
+
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //RaycastHit hit;
+
+        //if(Physics.Raycast(ray, out hit))
+        //{
+        //    if(hit.transform.tag == "slot")
+        //    {
+        //        print(hit.transform.name);
+        //    }
+        //}
+        
     }
+
+    void LateUpdate()
+    {
+        characterUIScreenPosition = Camera.main.ScreenToWorldPoint(inventoryCanvas.transform.position + new Vector3(0, -160f, 0.4f)); //+ new Vector3(0, -0.3f, 0.7f);
+        characterUI.transform.position = characterUIScreenPosition;
+
+        characterUI.transform.localEulerAngles = Camera.main.transform.localEulerAngles - new Vector3(0, 180, 0);
+    }
+
+    public void SaveInventory()
+    {
+        //Save Data here
+    }
+
+    public void LoadInventory()
+    {
+        //Load Data here
+    }
+
+    //public void AddItem(string id, int count)
+    //{
+    //    // 해당 이름의 아이템이 없다면 추가
+    //    if (items.ContainsKey(id) == false)
+    //        items.Add(id, 0);
+
+    //    // 아이템의 갯수를 증가시킨다.
+    //    items[id] += count;
+    //}
+    
+    //public void RemoveItem(string id, int count)
+    //{
+    //    //해당 이름의 아이템이 없다면 빠져나온다
+    //    if (items.ContainsKey(id) == false)
+    //        return;
+
+    //    //이름의 아이템이 있으면 갯수를 감소
+    //    items[id] -= count;
+
+    //    //갯수가 0개 이하가 되면 지운다.
+    //    if (items[id] < 0)
+    //        items.Remove(id);
+    //}
 }
