6489c833c41f6718c27fb45d0e5c35dce056773f
lMalaria <shinobi441@gmail.com>
2018-04-20 18:32:10 +0900

    Script for TitleScreen
    Play BGM using SoundManager's method

---
+++ Assets/Scripts/01.Menu/TitleScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource bgmSource;
+
+    [SerializeField]
+    private AudioClip bgmClip;
+
+	void Start ()
+    {
+        PlayBGM();
+	}
+	
+	void Update ()
+    {
+
+	}
+    
+    void PlayBGM()
+    {
+        SoundManager.Instance.PlayBGM(bgmSource, bgmClip, true, 0.4f);
+    }
+
+    void TitleScene()
+    {
+
+    }
+}
