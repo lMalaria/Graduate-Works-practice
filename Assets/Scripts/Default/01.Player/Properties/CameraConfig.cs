4b0b2af58aa1961cdb6902735e48a97ed46dd428
lMalaria <shinobi441@gmail.com>
2018-04-27 18:05:03 +0900

    CameraConfig class
    This is a script derived from ScriptableObject which make script can be attached like a gameObject

---
+++ Assets/Scripts/Default/01.Player/Properties/CameraConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Camera/Config")]
+public class CameraConfig : ScriptableObject
+{
+    public float turnSmooth;
+    public float pivotSpeed;
+    public float yAxisRotationSpeed;
+    public float xAxisRotationSpeed;
+    public float minAngle;
+    public float maxAngle;
+    public float normalZ;
+    public float normalX;
+    public float normalY;
+    public float aimX;
+    public float aimZ;
+}
