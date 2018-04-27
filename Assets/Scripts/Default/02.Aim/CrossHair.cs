1d569344cf561eaaee0242be992366c0812f11f7
lMalaria <shinobi441@gmail.com>
2018-04-27 18:02:36 +0900

    Cross Hair(aim) Script
    almost done

---
+++ Assets/Scripts/Default/02.Aim/CrossHair.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrossHair : MonoBehaviour {
+
+    [SerializeField]
+    private float currentSpread;
+
+    [SerializeField]
+    private float speedOfSpread;
+
+    [SerializeField]
+    private Parts[] parts;
+
+    [SerializeField]
+    PlayerController playerController;
+
+    [SerializeField]
+    CharacterStatus characterStatus;
+
+    private float t;
+
+    private float curSpread;
+	
+    void Start()
+    {
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
+    }
+
+	void Update()
+    {
+        if (playerController.movementAmount > 0)
+            currentSpread = 20 * (5 + playerController.movementAmount);
+        else
+            currentSpread = 20;
+
+        if (characterStatus.isAiming == true)
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+        else
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
+
+        UpdateCrossHair();
+	}
+
+    public void UpdateCrossHair()
+    {
+        t = 0.005f * speedOfSpread;
+        curSpread = Mathf.Lerp(curSpread, currentSpread, t);
+
+        for(int i = 0; i < parts.Length; i++)
+        {
+            Parts p = parts[i];
+
+            p.rect.anchoredPosition = p.pos * curSpread;
+
+        }
+    }
+}
+
+[System.Serializable]
+public class Parts
+{
+    public RectTransform rect;
+    public Vector2 pos;
+}
