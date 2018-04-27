5460fac8c98cb15d000d8a7ee44d3e8a95f875be
lMalaria <shinobi441@gmail.com>
2018-04-27 18:09:36 +0900

    CharacterInput Script
    To See precisely whether Script is going well or not, this is made

---
+++ Assets/Scripts/Default/01.Player/PlayerMove/CharacterInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour {
+
+    public CharacterStatus characterStatus;
+
+    [SerializeField]
+    private bool debugAiming;
+
+    [SerializeField]
+    private bool isAiming;
+
+	public void UpdateInput ()
+    {
+        if (!debugAiming)
+            characterStatus.isAiming = Input.GetMouseButton(1);
+        else
+            characterStatus.isAiming = isAiming;
+	}
+}
