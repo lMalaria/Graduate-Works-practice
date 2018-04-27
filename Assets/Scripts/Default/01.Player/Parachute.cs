f3406233f29714b1b0d58cc3f84644c18e9d5ff2
lMalaria <shinobi441@gmail.com>
2018-04-27 18:10:17 +0900

    Scene Script in Intro & Tutorial
    this script made CutScene

--- Assets/Scripts/Default/01.Player/Parachute.cs
+++ Assets/Scripts/Default/01.Player/Parachute.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Parachute : MonoBehaviour {
 
@@ -14,7 +12,7 @@ public class Parachute : MonoBehaviour {
     //[SerializeField]
     //Camera[] cameras;
 
-    void Awake()
+	void Start ()
     {
         //cameraObject = GameObject.Find("Camera");
         rigidbody = GetComponent<Rigidbody>();
@@ -22,13 +20,13 @@ public class Parachute : MonoBehaviour {
         // 연출을 위해서 3인칭 카메라와 중력적용을 꺼 놓은 채 시작 
         cameraObject.GetComponent<CameraFollow>().enabled = false;
         cameraObject.GetComponent<CutSceneCamera>().enabled = false;
+
+        GameObject.Find("swat").GetComponent<Animator>().enabled = false;
+        GameObject.Find("swat").GetComponent<Controller>().enabled = false;
+        GameObject.Find("CameraHolder").GetComponent<ThirdPersonCamera>().enabled = false;
+
         rigidbody.useGravity = false;
         rigidbody.constraints = RigidbodyConstraints.FreezeRotation; // 충돌시 물체가 돌아가지 않게 설정
-    }
-
-	void Start ()
-    {
-        //this.gameObject.transform.Rotate(90, 0, 0);
     }
 	
 	void Update ()
@@ -49,7 +47,46 @@ public class Parachute : MonoBehaviour {
         {
             rigidbody.drag = 0;
             cameraObject.GetComponent<CutSceneCamera>().enabled = false;
-            cameraObject.GetComponent<CameraFollow>().enabled = true;
+            cameraObject.GetComponent<CameraFollow>().enabled = false;
+            cameraObject.GetComponent<Camera>().depth = -2;
+            //GameObject.Find("swat").GetComponent<Animator>().enabled = true;
+            GameObject.Find("swat").GetComponent<Controller>().enabled = true;
+            GameObject.Find("CameraHolder").GetComponent<ThirdPersonCamera>().enabled = true;
+        }
+
+        if (this.gameObject.transform.position.y == 1.3126)
+        {
+            GameObject.Find("swat").GetComponent<Animator>().enabled = true;
+            //GameObject.Find("swat").GetComponent<Controller>().enabled = true;
+
+            //Destroy(GameObject.Find("swat").GetComponent<Parachute>());
+            //Destroy(this.gameObject.GetComponent<CutSceneCamera>());
+            //Destroy(cameraObject.GetComponent<Animator>());
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            //dialogueEndTime = 14;
+            //rigidbody.useGravity = true;
+
+            Vector3 arrivalPosition = new Vector3(3.6f, 1.3f, -2.535f); 
+            this.gameObject.transform.position = arrivalPosition;
+
+            cameraObject.GetComponent<Camera>().depth = -2;
+
+            GameObject.Find("swat").GetComponent<Animator>().enabled = true;
+            GameObject.Find("swat").GetComponent<Controller>().enabled = true;
+            GameObject.Find("CameraHolder").GetComponent<ThirdPersonCamera>().enabled = true;
+
+            Vector3 aimPivot = GameObject.Find("aim Pivot").transform.position;
+            aimPivot.y = aimPivot.y + 2000;
+            GameObject.Find("aim Pivot").transform.position = aimPivot;
+
+
+
+            Destroy(GameObject.Find("swat").GetComponent<Parachute>());
+            Destroy(this.gameObject.GetComponent<CutSceneCamera>());
+            Destroy(cameraObject.GetComponent<Animator>());
         }
 
 	}
