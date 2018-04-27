46e62d2a999582f6a6ced13bc40198a37789118e
lMalaria <shinobi441@gmail.com>
2018-04-27 18:06:50 +0900

    Controller Script
    This is a Handler which make script behave

---
+++ Assets/Scripts/Default/01.Player/PlayerMove/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public PlayerController playerController;
+    public CharacterAnimation characterAnimation;
+    public CharacterInput characterInput;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        playerController.MoveUpdate();
+        characterAnimation.UpdateAnimation();
+        characterInput.UpdateInput();
+    }
+
+    public void FixedUpdate()
+    {
+    }
+}
